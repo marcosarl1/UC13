382cfee38744c5e99d437617bcbd1042a6f60997
Marcos Júnior <marcosajr11@gmail.com>
2024-11-17 11:33:27 -0300

    feat: JSON-Server integration done!

---
+++ Atividade05/assets/js/form-handler.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+        const form = document.querySelector('form');
+        form.addEventListener('submit', async function (event) {
+            event.preventDefault();
+
+            const formData = {
+                nome: document.getElementById('nome').value,
+                email: document.getElementById('email').value,
+                dataEntrada: document.getElementById('entrada').value,
+                dataSaida: document.getElementById('saida').value,
+                adultos: parseInt(document.getElementById('adultos').value),
+                criancas: parseInt(document.getElementById('criancas').value),
+                obs: document.getElementById('observacoes').value
+            };
+
+            try {
+                const response = await fetch('http://localhost:3000/reservations',{
+                    method: 'POST',
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify(formData)
+                });
+
+                if (response.ok) {
+                    alert('Reserva enviada com sucesso!');
+                    form.reset();
+                } else {
+                    throw new Error('Erro ao enviar reserva');
+                }
+            } catch (error) {
+                alert('Erro ao enviar reserva: ' + error.message);
+            }
+        });
+    }
+)

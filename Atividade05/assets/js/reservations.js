3f6c7b8b4ea9185976b2420e583bd0d6bab8a1e9
Marcos Júnior <marcosajr11@gmail.com>
2024-11-17 11:59:40 -0300

    feat: reservations.js done

---
+++ Atividade05/assets/js/reservations.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', async function () {
+    const tableBody = document.getElementById('tableReservations');
+
+    try {
+        const response = await fetch('http://localhost:3000/reservations');
+        const reservations = await response.json();
+
+        reservations.forEach(reservations => {
+            const row = document.createElement('tr');
+
+            row.innerHTML = `
+                <td>${reservations.nome}</td>
+                <td>${reservations.email}</td>
+                <td>${new Date(reservations.dataEntrada).toLocaleDateString()}</td>
+                <td>${new Date(reservations.dataSaida).toLocaleDateString()}</td>
+                <td>${reservations.adultos}</td>
+                <td>${reservations.criancas}</td>
+                <td>${reservations.obs}</td>
+           `;
+            tableBody.appendChild(row);
+        });
+    } catch (error) {
+        console.error('Erro ao carregar reservas: ' + error)
+    }
+});
